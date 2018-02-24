db4e4de4a56a79fd952cf609b710675d5d6e6182
liuzhanliang <liuzhanliang@baidu.com>
2018-02-24 11:06:19 +0800

    functions for lemma resources

--- resources/lemma/dict_util.go
+++ resources/lemma/dict_util.go
@@ -4,7 +4,10 @@ import (
 	"bytes"
 	"compress/gzip"
 	"encoding/base64"
+	"fmt"
+	"github.com/golang/glog"
 	"github.com/liuzl/da"
+	"github.com/liuzl/segment"
 	"strings"
 )
 
@@ -14,17 +17,37 @@ func initDict(contents ...*string) error {
 	for _, c := range contents {
 		data, err := base64.StdEncoding.DecodeString(*c)
 		if err != nil {
+			glog.Error(err)
 			return err
 		}
 		reader, err := gzip.NewReader(bytes.NewReader(data))
 		if err != nil {
+			glog.Error(err)
 			return err
 		}
 		dict, err := da.Build(reader)
 		if err != nil {
+			glog.Error(err)
 			return err
 		}
 		cmnDicts = append(cmnDicts, dict)
+	}
+	Funcs["cmn"] = func(in []string) ([]string, error) {
+		inStr := strings.Join(in, "")
+		out, err := Convert(inStr)
+		if err != nil {
+			return nil, err
+		}
+		var ret []string
+		seg := segment.NewSegmenterDirect([]byte(out))
+		for seg.Segment() {
+			ret = append(ret, seg.Text())
+		}
+		if len(ret) == len(in) {
+			return ret, nil
+		} else {
+			return nil, fmt.Errorf("len(ret)!=len(in) of cmn lemma function")
+		}
 	}
 	return nil
 }
---
+++ resources/lemma/init.go
@@ -0,0 +1,68 @@
+package lemma
+
+import (
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
+	"fmt"
+	"github.com/golang/glog"
+	"io/ioutil"
+	"strings"
+)
+
+type lemmaFunc func([]string) ([]string, error)
+
+var Funcs map[string]lemmaFunc = make(map[string]lemmaFunc)
+var Dict map[string]map[string]string = make(map[string]map[string]string)
+
+func genDict(lang, body string) error {
+	data, err := base64.StdEncoding.DecodeString(body)
+	if err != nil {
+		glog.Error(err)
+		return err
+	}
+	reader, err := gzip.NewReader(bytes.NewReader(data))
+	if err != nil {
+		glog.Error(err)
+		return err
+	}
+	rawBytes, err := ioutil.ReadAll(reader)
+	if err != nil {
+		glog.Error(err)
+		return err
+	}
+	m := make(map[string]string)
+	for _, line := range strings.Split(string(rawBytes), "\n") {
+		if strings.TrimSpace(line) == "" {
+			continue
+		}
+		items := strings.Fields(line)
+		if len(items) != 3 || items[0] == items[1] {
+			if len(items) != 3 {
+				//glog.Warningf("line:[%s] len(items)!=3", line)
+			}
+			continue
+		}
+		m[items[1]] = items[0]
+	}
+	Dict[lang] = m
+	Funcs[lang] = func(in []string) ([]string, error) {
+		return dictMatch(in, m)
+	}
+	return nil
+}
+
+func dictMatch(in []string, m map[string]string) ([]string, error) {
+	if m == nil {
+		return nil, fmt.Errorf("lemma dict is nil")
+	}
+	var ret []string
+	for _, token := range in {
+		if str, has := m[token]; has {
+			ret = append(ret, str)
+		} else {
+			ret = append(ret, token)
+		}
+	}
+	return ret, nil
+}
