bdf81889cb07d9fef70cd5e0ce7fa7a0c8ac7483
liuzhanliang <liuzhanliang@baidu.com>
2018-02-26 17:19:45 +0800

    use TokenizePro instead of Tokenize

--- tokenizer.go
+++ tokenizer.go
@@ -34,11 +34,15 @@ func (self *Tokenizer) Process(d *Document) error {
 	}
 	var tokens []*Token
 	var pos int = 0
-	for i, word := range tokenizer.Tokenize(d.Text) {
+	for i, item := range tokenizer.TokenizePro(d.Text) {
+		word := item.Text
 		l := len([]byte(word))
 		token := &Token{Doc: d, Text: word, Type: Type(word),
 			I: i, StartByte: pos, EndByte: pos + l,
 			Annotations: map[string]string{"lowerd": strings.ToLower(word)}}
+		if item.Norm != "" {
+			token.Annotations["norm"] = item.Norm
+		}
 		pos += l
 		tokens = append(tokens, token)
 	}
