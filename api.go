3a70fabedc70a87a541c01dacedfdeefb21fa28e
liuzhanliang <zhanliangliu@qq.com>
2018-09-26 06:52:31 +0800

    add comments for exported funcs

--- api.go
+++ api.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 )
 
+// A Pipeline contains configured annotators and taggers for nl processing
 type Pipeline struct {
 	Annotators []string
 
 	taggers []Processor
 }
 
+// AddTagger adds a new processor t to Pipeline p
 func (p *Pipeline) AddTagger(t Processor) error {
 	if t == nil {
 		return fmt.Errorf("cannot add nil tagger!")
@@ -18,6 +20,7 @@ func (p *Pipeline) AddTagger(t Processor) error {
 	return nil
 }
 
+// Annotate tags the Document by each configured processors and taggers
 func (p *Pipeline) Annotate(d *Document) error {
 	err := Processors["_"].Process(d)
 	if err != nil {
@@ -37,6 +40,7 @@ func (p *Pipeline) Annotate(d *Document) error {
 	return nil
 }
 
+// NLP returns ling handler with the annotators
 func NLP(annotators ...string) (*Pipeline, error) {
 	for _, anno := range annotators {
 		if Processors[anno] == nil {
@@ -46,10 +50,13 @@ func NLP(annotators ...string) (*Pipeline, error) {
 	return &Pipeline{Annotators: annotators}, nil
 }
 
+// DefaultNLP returns ling handler with norm, lemma, unidecode and regex
 func DefaultNLP() (*Pipeline, error) {
 	return NLP("norm", "lemma", "unidecode", "regex")
 }
 
+// MustNLP is like NLP but panics if the annotators are not correct. It
+// simplifies safe initialization of global variables holding ling handler
 func MustNLP(annotators ...string) *Pipeline {
 	p, err := NLP(annotators...)
 	if err != nil {
